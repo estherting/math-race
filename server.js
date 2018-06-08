0949bd091623ac68c9c700e62a85ff8a87e9196b
Esther-Ting <estherjoyceting@gmail.com>
2018-06-08 11:05:04 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+var path = require("path");
+var bodyParser = require('body-parser');
+const app = express();
+app.use(express.static(__dirname + "/static"));
+app.use(bodyParser.urlencoded({ extended: true }));
+const server = app.listen(1337);
+const io = require('socket.io')(server);
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+var count = 0;
+
+io.on('connection', function (socket) {
+  socket.emit('greeting', { msg: 'Greetings, from server Node, brought to you by Sockets! -Server' }); //3
+  socket.on('thankyou', function (data) { //7
+    console.log(data.msg);
+  });
+});
+
+app.get('/', function(req, res) {
+  res.render("index");
+});
